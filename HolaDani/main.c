fa58eb937465509fb44ccb5d056f2194c2ab7f7f
Daniel Benítez Jiménez <dbenitezjimenez@iMac-de-Daniel.local>
2012-12-19 10:21:42 +0100

    Initial Commit

---
+++ HolaDani/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  HolaDani
+//
+//  Created by Daniel Benítez Jiménez on 19/12/12.
+//  Copyright (c) 2012 Daniel Benítez Jiménez. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
